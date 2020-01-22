42dce100ba8749ce187eadf7113e880c91ebe123
Michael Chen <michaelchengaming@gmail.com>
2020-01-22 05:10:53 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 // create your App component here
+
+import React, { Component } from "react";
+
+export class App extends Component {
+  async componentDidMount() {
+    const res = await fetch("http://api.open-notify.org/astros.json");
+    const data = res.json();
+    return data;
+  }
+
+  render() {
+    return <div>wubba lubba dub dub</div>;
+  }
+}
+
+export default App;
